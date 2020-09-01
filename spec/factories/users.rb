dae2f064adb8326008b04fe38511ee7618da204c
saku1219 <saku1219@gmail.com>
2020-09-01 20:50:00 +0900

    コメントアウト削除

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,6 @@ FactoryBot.define do
   factory :user do
     nickname {Faker::Name.last_name}
     email {Faker::Internet.free_email}
-    # email { "TEst123@gmail.com"}
     password { 'TESTtest1234' }
     password_confirmation {password}
     family_name { '山田' }
