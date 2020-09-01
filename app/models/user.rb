6d2d08b27769a55fef294ef64027f0f961299a51
saku1219 <saku1219@gmail.com>
2020-09-01 15:31:31 +0900

    バリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+    validates :nickname
+    validates :email, uniqueness: true, format: { with: /@/ }
+    validates :password, length: { minimum: 6 }, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
+    validates :family_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
+    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
+    validates :family_name_reading, format: { with: /\A[ァ-ヶー－]+\z/ }
+    validates :first_name_reading, format: { with: /\A[ァ-ヶー－]+\z/ }
+    validates :birthday
+  end
 end
