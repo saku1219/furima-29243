4277f23aeb4baed80aa02007ebe4ad53d952720c
saku1219 <saku1219@gmail.com>
2020-09-03 18:43:37 +0900

    itemモデルのテスト

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,10 +8,10 @@ class Item < ApplicationRecord
     validates :image
     validates :price,                 numericality: { only_integer: true, greater_than: 299, less_than: 10000000}
     validates :description
-    validates :category_id,           numericality: { other_than: 1, message: " is invalid" }
-    validates :condition_id,          numericality: { other_than: 1, message: " is invalid" }
-    validates :shipping_payer_id,     numericality: { other_than: 1, message: " is invalid" }
-    validates :shipping_from_area_id, numericality: { other_than: 1, message: " is invalid" }
-    validates :shipping_duration_id,  numericality: { other_than: 1, message: " is invalid" }
+    validates :category_id,           numericality: { other_than: 1, message: "is invalid" }
+    validates :condition_id,          numericality: { other_than: 1, message: "is invalid" }
+    validates :shipping_payer_id,     numericality: { other_than: 1, message: "is invalid" }
+    validates :shipping_from_area_id, numericality: { other_than: 1, message: "is invalid" }
+    validates :shipping_duration_id,  numericality: { other_than: 1, message: "is invalid" }
   end
 end
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :item do
-    
+    name { Faker::Lorem.words(number: 6) }
+    price { '2000' }
+    description { Faker::Lorem.words(number: 20) }
+    category_id { '2' }
+    condition_id { '2' }
+    shipping_payer_id { '2' }
+    shipping_from_area_id { '2' }
+    shipping_duration_id { '2' }
+    association :user
   end
 end
