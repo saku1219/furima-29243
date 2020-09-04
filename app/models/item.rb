be56cbfd07ae937c7a3a4f0b24784069c7561f83
saku1219 <saku1219@gmail.com>
2020-09-04 19:01:47 +0900

    商品詳細表示

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,10 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :shipping_payer
+  belongs_to_active_hash :shipping_from_area
+  belongs_to_active_hash :shipping_duration
 
   belongs_to :user 
   has_one :order
