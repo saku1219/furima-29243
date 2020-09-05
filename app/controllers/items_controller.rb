0dbd5ee457155d0683579bd8d9411892f61204b9
saku1219 <saku1219@gmail.com>
2020-09-05 14:20:13 +0900

    destroyアクションの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :set_item, only: [:show, :edit]
+  before_action :set_item, only: [:show, :edit, :destroy]
 
   def index
     @item = Item.all.order('created_at DESC')
@@ -28,9 +28,11 @@ class ItemsController < ApplicationController
   end
   
   def destroy
-    @item = Item.find(params[:id])
-    @item.destroy
+    if @item.destroy
     redirect_to root_path
+    else
+      render :show
+    end
   end
 
   private
